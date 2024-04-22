0deb6455f60d75f094ec74e4a7e9e24e9c7961d7
JOSEPLANES\fernando <fernandodomin.ruiz@murciaeduca.es>
2024-04-22 13:20:19 +0200

    inicio

---
+++ mvc/controllers/IndexController.php
@@ -0,0 +1,51 @@
+<?php
+class ItemsController
+{
+    function __construct()
+    {
+        //Creamos una instancia de nuestro mini motor de plantillas
+        $this->view = new View();
+    }
+ 
+    public function listar()
+    {
+        //Incluye el modelo que corresponde
+        require 'models/ItemsModel.php';
+ 
+        //Creamos una instancia de nuestro "modelo"
+        $items = new ItemsModel();
+ 
+        //Le pedimos al modelo todos los items
+        $listado = $items->getAll();
+ 
+        //Pasamos a la vista toda la información que se desea representar
+        $data['items'] = $listado;
+ 
+        //Finalmente presentamos nuestra plantilla
+        $this->view->show("listar.php", $data);
+    }
+ 
+ 
+    public function index()
+    {
+        //Incluye el modelo que corresponde
+        require 'models/ItemsModel.php';
+ 
+        //Creamos una instancia de nuestro "modelo"
+        $items = new ItemsModel();
+ 
+        //Le pedimos al modelo todos los items
+        $listado = $items->getAll();
+ 
+        //Pasamos a la vista toda la información que se desea representar
+        $data['items'] = $listado;
+ 
+        //Finalmente presentamos nuestra plantilla
+        $this->view->show("listar.php", $data);
+    }
+    public function agregar()
+    {
+        echo 'Aquí incluiremos nuestro formulario para insertar items';
+    }
+}
+?>
